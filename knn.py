1448e6c9890859ec9ff45f0bf1e8dea8affde49c
김성태 <hkbgff@gmail.com>
2023-11-10 10:08:59 +0900

    수정

--- knn.py
+++ knn.py
@@ -15,32 +15,6 @@ def remove_outliers_iqr(data, column):
     data = data[(data[column] >= lower_bound) & (data[column] <= upper_bound)]
     return data
 
-def plot_and_save_graph(data_scaled, input_data_scaled, indices, filename='nearest_countries.png'):
-    plt.figure(figsize=(8, 6))
-
-    # 모든 국가 데이터 점으로 표시
-    plt.scatter(data_scaled[:, 0], data_scaled[:, 1], label='All Countries', alpha=0.5)
-
-    # 입력값 표시
-    plt.scatter(input_data_scaled[:, 0], input_data_scaled[:, 1], color='red', label='Input Data', marker='x')
-
-    # 가장 가까운 국가 3개 표시
-    for i in range(3):
-        plt.scatter(data_scaled[indices[0][i], 0], data_scaled[indices[0][i], 1], label=f'Nearest Country {i+1}', marker='o')
-
-    # 그래프 레이블과 범례 추가
-    plt.xlabel('Total Expense (Scaled)')
-    plt.ylabel('Average Day of Staying (Scaled)')
-    plt.title('Nearest Countries to Input Data')
-    plt.legend()
-
-    # 그래프 저장
-    plt.savefig(filename)
-
-    # 그래프 표시
-    plt.show()
-
-
 
 # 데이터 전처리 및 모델 초기화
 class KNNModel:
@@ -73,8 +47,6 @@ class KNNModel:
         input_data_scaled = self.scaler.transform(input_data_df)
         distances, indices = self.model.kneighbors(input_data_scaled)
 
-        # 그래프 그리기 및 저장
-        plot_and_save_graph(self.data_scaled, input_data_scaled, indices)
         
         return self.data.iloc[indices[0]]
 
